bf9cafacd02a42536a6eedc49242eaef2066b1ae
borisap <borisap@spotadvert.com>
2021-05-29 19:51:36 +0300

    test 2 created

---
+++ JavaSoftUni/src/NewTestJava.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class NewTestJava {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = Integer.parseInt(scan.nextLine());
+
+        System.out.printf("Line of code: %d", number);
+    }
+}
